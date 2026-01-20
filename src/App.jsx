5625bc5fdae9f6cf1127e0f62177be817ceafd00
somagouda <madagoudasomagouda@gmail.com>
2026-01-20 14:02:54 +0530

    Initial commit of SamEstate  project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function App() {
+  return (
+   
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-red-500 text-6xl font-bold">
+        TAILWIND WORKING 🔥
+      </h1>
+    </div>
+  
+  )
+}
