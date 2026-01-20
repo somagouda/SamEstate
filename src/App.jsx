59d257e632219e92ae42a167c9ef97cf03588b01
somagouda <madagoudasomagouda@gmail.com>
2026-01-20 14:45:45 +0530

    Creating Pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
 
+import{ BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from './pages/about.jsx'
+import Profile from './pages/Profile.jsx'
+import SignIn from './pages/Signin.jsx'
+import SignUp from './pages/SignUp.jsx'
 export default function App() {
-  return (
-   
-    <div className="min-h-screen bg-black flex items-center justify-center">
-      <h1 className="text-red-500 text-6xl font-bold">
-        TAILWIND WORKING 🔥
-      </h1>
-    </div>
-  
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/about' element={<About/>}/>
+    <Route path='/profile' element={<Profile/>}/>
+    <Route path='/sign-in' element={<SignIn/>}/>
+    <Route path='/sign-up' element={<SignUp/>}/>
+  </Routes>
+  </BrowserRouter>
 }
