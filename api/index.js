b42a5706c6391389071366872de211c9b69a960e
somagouda <madagoudasomagouda@gmail.com>
2026-01-21 17:49:26 +0530

    connect mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const mongoUri = process.env.MONGO;
+if (mongoUri) {
+  mongoose.connect(mongoUri)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.warn('MongoDB connection failed:', err.message));
+} else {
+  console.warn('MONGO env variable not set - running without database');
+}
+
 const app = express();
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
-}
-);
+});
