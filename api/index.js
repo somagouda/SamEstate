f09f8f96642acec39a81576399130e679e3edf9d
somagouda <madagoudasomagouda@gmail.com>
2026-01-22 15:23:58 +0530

    api route

--- api/index.js
+++ api/index.js
@@ -1,20 +1,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import userRoutes from './routes/user.route.js';
 dotenv.config();
-
-const mongoUri = process.env.MONGO;
-if (mongoUri) {
-  mongoose.connect(mongoUri)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.warn('MongoDB connection failed:', err.message));
-} else {
-  console.warn('MONGO env variable not set - running without database');
-}
-
-const app = express();
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("connected to mongoDB");
+}).catch((err)=>{
+    console.log(err);
 });
+const app=express();
+app.use(express.json());
+app.listen(3000,()=>{
+    console.log("server is running on port 3000!!!!");
+});
+app.get('/test', (req, res) => {
+  res.json({ message: 'Hello World!' });
+});
+
+app.use("/api/user", userRoutes);
